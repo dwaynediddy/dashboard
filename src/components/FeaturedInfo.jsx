bf1de5c5c80a5f13e23272afa09698383976610c
dwaynediddy <burrows.dwayne@icloud.com>
2021-12-08 00:28:57 +0100

    finished widgets on home dashboard still one small issue will come back to

--- src/components/FeaturedInfo.jsx
+++ src/components/FeaturedInfo.jsx
@@ -1,16 +1,44 @@
+import { useState, useEffect } from 'react'
 import './featuredInfo.css'
+import { userRequest } from '.././redux/requestMethods'
+
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
 
 const FeaturedInfo = () => {
+    const [income, setIncome] = useState([])
+    const [percentage, setPercentage] = useState(0)
+
+useEffect(() => {
+    const getIncome = async () => {
+        try {
+            const res = await userRequest.get('orders/income')
+            setIncome(res.data)
+            setPercentage((res.data[1].amount *100) / res.data[0].amount -100)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+        getIncome()
+}, [])
+
+console.log(income)
+console.log(percentage) 
+
     return (
         <div className="featured">
             <div className="featuredItem">
                 <span className="featuredTitle">Revenue</span>
                 <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">$52,596  </span>
+                    {/* income not rendering */}
+                    <span className="featuredMoney">${income[1]}</span>
                     <span className="featuredMoneyRate">
-                        -217 <ArrowDownwardIcon className="featuredIcon negative" />
+                        %{Math.floor(percentage)}{''}
+                        {percentage < 0 ? (
+                            <ArrowDownwardIcon className="featuredIcon negative" />
+                            ) : (
+                            <ArrowUpwardIcon className="featuredIcon" />
+                         )}
                     </span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
