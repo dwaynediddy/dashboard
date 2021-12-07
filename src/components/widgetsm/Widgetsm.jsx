f2c228ebb1febf1fac4ea5a5dc9c11fd10931c3a
dwaynediddy <burrows.dwayne@icloud.com>
2021-12-07 16:40:47 +0100

    added users from database to display

--- src/components/widgetsm/Widgetsm.jsx
+++ src/components/widgetsm/Widgetsm.jsx
@@ -1,18 +1,33 @@
+import { useState, useEffect } from 'react'
 import './widgetsm.css'
 
-import Avatar from '../../images/avatar.png'
-
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { userRequest } from '../../redux/requestMethods'
 
 const Widgetsm = () => {
+    const [users, setUsers] = useState([])
+
+useEffect(() => {
+    const getUsers = async(req, res) => {
+        try {
+            const res = await userRequest.get('users/?new=true')
+            setUsers(res.data)
+        } catch (err) {
+            console.log('error with widget sm user res')
+        }
+    }
+        getUsers()
+    }, [])
+
     return (
         <div className="widgetsm">
             <h3 className="widgetSmTitle">New Members</h3>
             <ul className="widgetSmList">
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
+                {users.map(user => (
+                    <li className="widgetSmListItem" key={user._id}>
+                    <img src={user.image || 'https://boznoslawoffice.com/wp-content/uploads/2015/09/no_avatar.jpg' } alt="users avatar" className="widgetSmImg" />
                     <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
+                        <span className="widgetSmUsername">{user.username}</span>
                         <span className="widgetSmUserTitle">Software Engineer</span>
                     </div>
                     <button className="widgetSmBtn">
@@ -20,61 +35,7 @@ const Widgetsm = () => {
                         Display
                     </button>
                 </li>
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmBtn">
-                        <VisibilityIcon className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmBtn">
-                        <VisibilityIcon className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmBtn">
-                        <VisibilityIcon className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmBtn">
-                        <VisibilityIcon className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
-                <li className="widgetSmListItem">
-                    <img src={Avatar} alt="users avatar" className="widgetSmImg" />
-                    <div className="widgetSmUser">
-                        <span className="widgetSmUsername">bob the builder</span>
-                        <span className="widgetSmUserTitle">Software Engineer</span>
-                    </div>
-                    <button className="widgetSmBtn">
-                        <VisibilityIcon className="widgetSmIcon" />
-                        Display
-                    </button>
-                </li>
+            ))}
             </ul>
         </div>
     )
